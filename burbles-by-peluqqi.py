6f3d65daf44a66bdf8c370ad55cb3da56d7f54a6
Antonio Jesús Criado Manzaneque <aj.criman@gmail.com>
2023-11-05 11:53:51 +0100

    Code refactor and new methods

--- burbles-by-peluqqi.py
+++ burbles-by-peluqqi.py
@@ -3,11 +3,69 @@ import os
 import time
 
 class bcolors:
+    DEFAULT="\033[0;0m"
     DARKORANGE="\033[38;5;208m"
     SOFTORANGE="\033[38;5;180m"
-    DEFAULT="\033[0;0m"
     DARKBLUE="\033[38;5;68m"
+    REDWARNING="\033[38;5;167m"
     GREY="\033[38;5;234m"
+
+class commands:
+    PRIVESC_ENVENUM=[
+        "whoami",
+        "id",
+        "hostname",
+        "ip a",
+        "sudo -l",
+        "cat /etc/os-release",
+        "echo $PATH",
+        "env",
+        "uname -a",
+        # "lscpu",
+        "cat /etc/shells",
+        # "cat /etc/fstab | grep -v \"#\" | column -t",
+        # "route",
+        # "arp -a",
+        "cat /etc/passwd",
+        "cat /etc/shadow",
+        "cat /etc/group",
+        "ls /home",
+        "df -h", # Mounted fs
+        "find / -type d -name \".*\" -ls 2>/dev/null",
+        "ls -l /tmp /var/tmp /dev/shm"
+    ]
+    PRIVESC_INTENUM=[
+        "ip a",
+        "cat /etc/hosts",
+        "lastlog",
+        "w",
+        "history",
+        "find / -type f \( -name *_hist -o -name *_history \) -exec ls -l {} \; 2>/dev/null",
+        "ls -R -la /etc/cron*"
+        #"find /proc -name cmdline -exec cat {} \; 2>/dev/null | tr \" \" \"\\n\""
+    ]
+    PRIVESC_SERVENUM=[
+        "apt list --installed | tr \"/\" \" \" | cut -d\" \" -f1,3 | sed 's/[0-9]://g'",
+        "sudo -V | head -n 1",
+        # "ls -l /bin /usr/bin/ /usr/sbin/"
+        "find / -type f \( -name *.conf -o -name *.config \) -exec ls -l {} \; 2>/dev/null",
+        "find / -type f -name \"*.sh\" 2>/dev/null | grep -v \"src\|snap\|share\"",
+        "ps aux | grep root",
+    ]
+    PRIVESC_CREDHUNTING=[
+        "ls /var",
+        "ls ~/.ssh",
+        "find / -type f -name *.ssh -exec ls -l {} \; 2>/dev/null",
+        "find / -type f -name *.bak -exec ls -l {} \; 2>/dev/null",
+    ]
+
+def color(str, code):
+    return f"{code}{str}{bcolors.DEFAULT}"
+
+def processCommand(cmd):
+    choice = input("\n"+color("[>] "+cmd,bcolors.SOFTORANGE)+" ")
+    if choice == "":
+        os.system(cmd)
 
 def printBanner():
     print("======================================================================================");
@@ -39,97 +97,68 @@ def mainMenu():
 def linPrivescMenu():
     print("\nLinux Privilege Escalation:");
     print("1) Environment enumeration");
-    print("2) Services and internals enumeration");
-    print("3) Back to main menu");
+    print("2) Internals enumeration");
+    print("3) Services enumeration");
+    print("4) Credential hunting");
+    print("5) Back to main menu");
+
     choice = input("\n[>] ");
     if choice == "1":
         envEnumeration()
     elif choice == "2":
-        servEnumeration()
+        intEnumeration()
     elif choice == "3":
+        servEnumeration()
+    elif choice == "4":
+        credHunting()
+    elif choice == "5":
         mainMenu()
     else:
         linPrivescMenu()
 
 def envEnumeration():
     user = os.popen('whoami').read()
-    print(color("\n===== ENVIRONMENT ENUMERATION ========================================================\n",bcolors.DARKBLUE));
-    print(color("[>] whoami",bcolors.SOFTORANGE))
-    print(f"{user}")
-    print(color("[>] id",bcolors.SOFTORANGE))
-    os.system("id")
-    print(color("\n[>] hostname",bcolors.SOFTORANGE))
-    os.system("hostname")
-    print(color("\n[>] ip a",bcolors.SOFTORANGE))
-    os.system("ip a")
-    print(color("\n[>] sudo -l",bcolors.SOFTORANGE))
-    choice = input("You may need to introduce a password, do you want to continue? (y/N) ");
-    if choice == "y" or choice == "Y" or choice == "YES" or choice == "yes":
-        os.system("sudo -l")
-    print(color("\n[>] cat /etc/os-release",bcolors.SOFTORANGE))
-    os.system("cat /etc/os-release")
-    print(color("\n[>] echo $PATH",bcolors.SOFTORANGE))
-    os.system("echo $PATH")
-    print(color("\n[>] env",bcolors.SOFTORANGE))
-    os.system("env")
-    print(color("\n[>] uname -a",bcolors.SOFTORANGE))
-    os.system("uname -a")
-    print(color("\n[>] lscpu",bcolors.SOFTORANGE))
-    os.system("lscpu")
-    print(color("\n[>] cat /etc/shells",bcolors.SOFTORANGE))
-    os.system("cat /etc/shells")
-    print(color("\n[>] cat /etc/fstab | grep -v \"#\" | column -t",bcolors.SOFTORANGE))
-    os.system("cat /etc/fstab | grep -v \"#\" | column -t")
-    print(color("\n[>] route",bcolors.SOFTORANGE))
-    os.system("route")
-    print(color("\n[>] arp -a",bcolors.SOFTORANGE))
-    os.system("arp -a")
-    print(color("\n[>] cat /etc/passwd",bcolors.SOFTORANGE))
-    os.system("cat /etc/passwd")
-    print(color("\n[>] cat /etc/shadow",bcolors.SOFTORANGE))
-    os.system("cat /etc/shadow")
-    print(color("\n[>] cat /etc/group",bcolors.SOFTORANGE))
-    os.system("cat /etc/group")
-    print(color("\n[>] ls /home",bcolors.SOFTORANGE))
-    os.system("ls /home")
-    print(color("\n[>] df -h",bcolors.SOFTORANGE))
-    os.system("df -h")
-    print(color("\n[>] find / -type f -name \".*\" -exec ls -l {} \; 2>/dev/null | grep "+user,bcolors.SOFTORANGE))
-    os.system("find / -type f -name \".*\" -exec ls -l {} \; 2>/dev/null | grep "+user)
-    print(color("\n[>] find / -type d -name \".*\" -ls 2>/dev/null",bcolors.SOFTORANGE))
-    os.system("find / -type d -name \".*\" -ls 2>/dev/null")
-    print(color("\n[>] ls -l /tmp /var/tmp /dev/shm",bcolors.SOFTORANGE))
-    os.system("ls -l /tmp /var/tmp /dev/shm")    
+    print(color("\n===== ENVIRONMENT ENUMERATION ===============================================================\n",bcolors.DARKBLUE));
+    print("Press enter to execute")
+    
+    for cmd in commands.PRIVESC_ENVENUM:
+        processCommand(cmd)
 
-    print(color("\n===== END OF ENVIRONMENT ENUMERATION =================================================",bcolors.DARKBLUE));
+    processCommand("find / -type f -name \".*\" -exec ls -l {} \; 2>/dev/null | grep "+user)
+    print(color("\n===== END OF ENVIRONMENT ENUMERATION =========================================================",bcolors.DARKBLUE));
+    mainMenu()
+
+def intEnumeration():
+    print(color("\n===== INTERNALS ENUMERATION ==================================================================\n",bcolors.DARKBLUE));
+    print("Press enter to execute")
+    
+    for cmd in commands.PRIVESC_INTENUM:
+        processCommand(cmd)
+
+    print(color("\n===== END OF INTERNALS ENUMERATION ===========================================================",bcolors.DARKBLUE));
     mainMenu()
 
 def servEnumeration():
-    print(""+color("\n===== SERVICES AND INTERNALS ENUMERATION =============================================",bcolors.DARKBLUE)+"\n");
-    print(color("\n[>] ip a",bcolors.SOFTORANGE))
-    os.system("ip a")
-    print(color("\n[>] cat /etc/hosts",bcolors.SOFTORANGE))
-    os.system("cat /etc/hosts")
-    print(color("\n[>] lastlog",bcolors.SOFTORANGE))
-    os.system("lastlog")
-    print(color("\n[>] w",bcolors.SOFTORANGE))
-    os.system("w")
-    print(color("\n[>] history",bcolors.SOFTORANGE))
-    os.system("history")
-    print(color("\n[>] find / -type f \( -name *_hist -o -name *_history \) -exec ls -l {} \; 2>/dev/null",bcolors.SOFTORANGE))
-    os.system("find / -type f \( -name *_hist -o -name *_history \) -exec ls -l {} \; 2>/dev/null")
-    print(color("\n[>] ls -R -la /etc/cron*",bcolors.SOFTORANGE))
-    os.system("ls -R -la /etc/cron*")
-    """print(color("\n[>] ip a",bcolors.SOFTORANGE))
-    os.system("ip a")
-    print(color("\n[>] ip a",bcolors.SOFTORANGE))
-    os.system("ip a")"""
+    print(color("\n===== SERVICES ===============================================================================",bcolors.DARKBLUE)+"\n");
+    print("Press enter to execute")
+    
+    for cmd in commands.PRIVESC_SERVENUM:
+        processCommand(cmd)
 
-    print(color("\n===== END OF SERVICES AND INTERNALS ENUMERATION =========================================",bcolors.DARKBLUE));
+    print(color("\nDo not forget to run pspy to check all processes",bcolors.REDWARNING));
+    print(color("\n===== END OF SERVICES ENUMERATION ============================================================",bcolors.DARKBLUE));
     mainMenu()
 
-def color(str, code):
-    return f"{code}{str}{bcolors.DEFAULT}"
+
+def credHunting():
+    print(color("\n===== CREDENTIAL HUNTING =====================================================================",bcolors.DARKBLUE)+"\n");
+    print("Press enter to execute")
+    
+    for cmd in commands.PRIVESC_CREDHUNTING:
+        processCommand(cmd)
+
+    print(color("\n===== END OF CREDENTIAL HUNTING ==============================================================",bcolors.DARKBLUE));
+    mainMenu()
 
 if __name__ == "__main__":
     try:
